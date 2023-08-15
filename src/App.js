3d817406d0c15895b99b9324611ec378c6a8498e
teachingShai <teachingshai@gmail.com>
2023-08-15 11:40:19 +0200

    test5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Nav";
+
 
 function App() {
+  const firstname = "Zvika";
+  const lastname = "brich";
+
+  function NumberRangeChecker(num) {
+    if (num <= 50) {
+      return <div>the number: {num} is smaller then 50</div>;
+    } else if (num > 50 && num <= 100) {
+      return <div>the number: {num} is between 50 and 100</div>;
+    } else {
+      return <div>the number: {num} larger then 100</div>;
+    }
+  }
+  const ans = NumberRangeChecker(56);
+
+  function fullName() {
+    return firstname + " " + lastname;
+  }
+  const fn = fullName();
+
+  function displayNums(num) {
+    let elements = [];
+    for (let i = 0; i < num; i++) {
+      elements.push(<div>{i}</div>);
+      //span - display inline - לא יורד שורה
+      // div  display block יורד שורה is container
+      //<p> display block  יורד שורה
+    }
+    return <div>{elements}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Details</h1>
+      <Nav/>
+      {/* <p>My name is {firstname}</p>
+            <p>My name is {lastname}</p> */}
+      {fn}
+      {displayNums(100)}
+      {NumberRangeChecker(56)}
     </div>
   );
 }
